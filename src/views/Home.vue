e820f6cd27d0ec7c28a3eef269448c6905b2d38b
bogdan <bogdandihtyar@gmail.com>
2020-12-12 15:27:29 +0200

    Buttons and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="home">
+
       <h1>Home page</h1>
-      <p class="red white--text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio hic cupiditate accusantium voluptates ullam sunt numquam consequuntur tempora sapiente. Error soluta fugit rem harum cum illum maxime eius optio quos.</p>
-      <p class="pink lighten-4 red--text text-darken-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio hic cupiditate accusantium voluptates ullam sunt numquam consequuntur tempora sapiente. Error soluta fugit rem harum cum illum maxime eius optio quos.</p>
-      <h2 class="display-4">Masive Display</h2>
-      <h3 class="display-1">Smaller Display</h3>
+      <v-btn class="pink white--text">Click me</v-btn>
+      <v-btn color="green" depressed>Click me</v-btn>
+      <v-btn color="yellow" flat>Click me</v-btn>
+      <v-btn flat color="pink" >Click me</v-btn>
+
+      <v-btn depressed class="blue white--text">
+        <v-icon left>email</v-icon>
+        <span>Email me</span>
+      </v-btn>
+
+      <v-btn depressed small class="blue white--text">
+        <v-icon left small>email</v-icon>
+        <span>Email me</span>
+      </v-btn>
+
+      <v-btn depressed large class="blue white--text">
+        <span>Email me</span>
+        <v-icon right large>email</v-icon>
+      </v-btn>
+
+      <v-btn fab depressed small dark color="purple">
+        <v-icon>favorite</v-icon>
+      </v-btn>
+      
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'Home',
   components: {
